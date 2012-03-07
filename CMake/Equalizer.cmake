bf40758b1fcb55cccda091216c873accc5a539f2
Stefan Eilemann <stefan.eilemann@epfl.ch>
2012-03-07 16:14:36 +0100

    Add dependency support

--- CMake/Equalizer.cmake
+++ CMake/Equalizer.cmake
@@ -1,6 +1,7 @@
 # -*- mode: cmake -*-
 
 set(EQUALIZER_VERSION 1.3.1)
+set(EQUALIZER_DEPENDS gpu-sd)
 
 #set(EQUALIZER_REPO_TYPE git)
 set(EQUALIZER_REPO_URL https://github.com/Eyescale/Equalizer.git)
