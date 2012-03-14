38d71f2d262d6b5f65c2ed547a688ea493ec0ca9
Stefan Eilemann <eilemann@gmail.com>
2012-03-14 11:12:37 +0100

    Change git clone script to avoid excessive cloning of git sources

--- CMake/UseExternal.cmake
+++ CMake/UseExternal.cmake
@@ -2,6 +2,74 @@
 include(ExternalProject)
 find_package(Git REQUIRED)
 
+# overwrite git clone script generation to avoid excessive cloning
+function(_ep_write_gitclone_script script_filename source_dir git_EXECUTABLE git_repository git_tag src_name work_dir)
+  file(WRITE ${script_filename}
+"if(\"${git_tag}\" STREQUAL \"\")
+  message(FATAL_ERROR \"Tag for git checkout should not be empty.\")
+endif()
+if(IS_DIRECTORY \"${work_dir}/${src_name}/.git\")
+  execute_process(
+    COMMAND \"${git_EXECUTABLE}\" checkout ${git_tag}
+    WORKING_DIRECTORY \"${work_dir}/${src_name}\"
+    RESULT_VARIABLE error_code
+    )
+  if(error_code)
+    message(FATAL_ERROR \"Failed to checkout ${git_tag} in '${source_dir}'\")
+  endif()
+else()
+  execute_process(
+    COMMAND \${CMAKE_COMMAND} -E remove_directory \"${source_dir}\"
+    RESULT_VARIABLE error_code
+    )
+  if(error_code)
+    message(FATAL_ERROR \"Failed to remove directory: '${source_dir}'\")
+  endif()
+
+  execute_process(
+    COMMAND \"${git_EXECUTABLE}\" clone \"${git_repository}\" \"${src_name}\"
+    WORKING_DIRECTORY \"${work_dir}\"
+    RESULT_VARIABLE error_code
+    )
+  if(error_code)
+    message(FATAL_ERROR \"Failed to clone repository: '${git_repository}'\")
+  endif()
+
+  execute_process(
+    COMMAND \"${git_EXECUTABLE}\" checkout ${git_tag}
+    WORKING_DIRECTORY \"${work_dir}/${src_name}\"
+    RESULT_VARIABLE error_code
+    )
+  if(error_code)
+    message(FATAL_ERROR \"Failed to checkout tag: '${git_tag}'\")
+  endif()
+endif()
+
+execute_process(
+  COMMAND \"${git_EXECUTABLE}\" submodule init
+  WORKING_DIRECTORY \"${work_dir}/${src_name}\"
+  RESULT_VARIABLE error_code
+  )
+if(error_code)
+  message(FATAL_ERROR \"Failed to init submodules in: '${work_dir}/${src_name}'\")
+endif()
+
+execute_process(
+  COMMAND \"${git_EXECUTABLE}\" submodule update --recursive
+  WORKING_DIRECTORY \"${work_dir}/${src_name}\"
+  RESULT_VARIABLE error_code
+  )
+if(error_code)
+  message(FATAL_ERROR \"Failed to update submodules in: '${work_dir}/${src_name}'\")
+endif()
+
+"
+)
+
+endfunction(_ep_write_gitclone_script)
+
+
+
 function(USE_EXTERNAL_GATHER_ARGS NAME)
   # sets ${UPPER_NAME}_ARGS on return, to be passed to CMake
   string(TOUPPER ${NAME} UPPER_NAME)
