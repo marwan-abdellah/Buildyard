aa1bdc63b1c353a3b7963df42d2f150e14e4b442
Stefan Eilemann <eilemann@gmail.com>
2012-03-14 11:48:04 +0100

    Make dash url windows-friendly

--- config/dash.cmake
+++ config/dash.cmake
@@ -1,5 +1,5 @@
 
 set(DASH_VERSION 0.1.0)
 set(DASH_DEPENDS Equalizer)
-set(DASH_REPO_URL git@github.com:BlueBrain/dash.git)
+set(DASH_REPO_URL https://github.com/BlueBrain/dash.git)
 set(DASH_REPO_TAG master)
