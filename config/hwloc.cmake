afa6c4249c82a8ff8674fbb52712731a2fe66334
Stefan Eilemann <eilemann@gmail.com>
2012-03-21 11:17:39 +0100

    Partial Darwin build fix

--- config/hwloc.cmake
+++ config/hwloc.cmake
@@ -7,6 +7,6 @@ set(HWLOC_REPO_URL http://svn.open-mpi.org/svn/hwloc/tags/hwloc-1.4.0/)
 set(HWLOC_REPO_TYPE SVN)
 set(HWLOC_SOURCE "${CMAKE_SOURCE_DIR}/src/hwloc")
 set(HWLOC_EXTRA
-  PATCH_COMMAND cd ${HWLOC_SOURCE} && autoreconf -ivf
+  PATCH_COMMAND cd ${HWLOC_SOURCE} && env PATH=/opt/local/bin:${PATH} autoreconf -ivf
   CONFIGURE_COMMAND ${HWLOC_SOURCE}/configure  "--prefix=${CMAKE_CURRENT_BINARY_DIR}/install"
 )
