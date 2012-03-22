87661d453b854a03dcbaf79dea99b237a47a9baa
Stefan Eilemann <eilemann@gmail.com>
2012-03-22 13:21:35 +0100

    Windows fixes

--- config/Boost.cmake
+++ config/Boost.cmake
@@ -3,8 +3,17 @@ set(BOOST_SOURCE "${CMAKE_SOURCE_DIR}/src/Boost")
 set(BOOST_VERSION 1.42.0)
 set(BOOST_REPO_URL http://svn.boost.org/svn/boost/tags/release/Boost_1_49_0)
 set(BOOST_REPO_TYPE SVN)
+
+if(MSVC)
+  set(BOOTSTRAP cd ${BOOST_SOURCE} && bootstrap.bat "--prefix=${CMAKE_CURRENT_BINARY_DIR}/install" --with-libraries=serialization,system,regex,date_time)
+  set(BTWO b2)
+else()
+  set(BOOTSTRAP cd ${BOOST_SOURCE} && ./bootstrap.sh "--prefix=${CMAKE_CURRENT_BINARY_DIR}/install" --with-libraries=serialization,system,regex,date_time)
+  set(BTWO ./b2)
+endif()
+
 set(BOOST_EXTRA
-  CONFIGURE_COMMAND cd ${BOOST_SOURCE} && ./bootstrap.sh "--prefix=${CMAKE_CURRENT_BINARY_DIR}/install" --with-libraries=serialization,system,regex,date_time
-  BUILD_COMMAND cd ${BOOST_SOURCE} && ./b2 -j8
-  INSTALL_COMMAND cd ${BOOST_SOURCE} && ./b2 install
+  CONFIGURE_COMMAND ${BOOTSTRAP}
+  BUILD_COMMAND cd ${BOOST_SOURCE} && ${BTWO} -j8
+  INSTALL_COMMAND cd ${BOOST_SOURCE} && ${BTWO} install
 )
