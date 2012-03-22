a24245056bc89cb43a3d944d06bd15b65dd492a0
Stefan Eilemann <eilemann@gmail.com>
2012-03-22 08:09:22 +0100

    Disable usage of hwloc on OS X

--- config/hwloc.cmake
+++ config/hwloc.cmake
@@ -1,10 +1,7 @@
-if(MSVC) # Can't autopain on Windows without too much hassle
+# can't autopain on Windows without too much hassle
+# can't build universal on OS X
+if(MSVC OR APPLE)
   return()
-endif()
-if(APPLE)
-  set(AUTORECONF /opt/local/bin/autoreconf)
-else()
-  set(AUTORECONF autoreconf)
 endif()
 
 set(HWLOC_VERSION 1.4.0)
@@ -12,6 +9,6 @@ set(HWLOC_REPO_URL http://svn.open-mpi.org/svn/hwloc/tags/hwloc-1.4.0/)
 set(HWLOC_REPO_TYPE SVN)
 set(HWLOC_SOURCE "${CMAKE_SOURCE_DIR}/src/hwloc")
 set(HWLOC_EXTRA
-  PATCH_COMMAND cd ${HWLOC_SOURCE} && env ${AUTORECONF} -i
+  PATCH_COMMAND cd autoreconf -i
   CONFIGURE_COMMAND ${HWLOC_SOURCE}/configure  "--prefix=${CMAKE_CURRENT_BINARY_DIR}/install"
 )
