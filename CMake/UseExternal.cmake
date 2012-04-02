0d373069dd3b1d101fa52fd3003b631e701ca132
Stefan Eilemann <eilemann@gmail.com>
2012-04-02 10:29:09 +0200

    Tweaks for bamboo

--- CMake/UseExternal.cmake
+++ CMake/UseExternal.cmake
@@ -7,7 +7,7 @@ find_package(Subversion REQUIRED)
 set_property(GLOBAL PROPERTY USE_FOLDERS ON)
 
 set(USE_EXTERNAL_SUBTARGETS update build buildonly configure test install
-  package doxygen)
+  package doxygen download)
 foreach(subtarget ${USE_EXTERNAL_SUBTARGETS})
   add_custom_target(${subtarget}s)
   set_target_properties(${subtarget}s PROPERTIES FOLDER "00_Meta")
