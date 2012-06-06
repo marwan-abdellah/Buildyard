ba215e8995cb1e28c30ffe64dc84fa375fee2a85
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2012-06-06 11:50:38 +0200

    Add RealityGrid steering lib & client

---
+++ config/qt_steerer.cmake
@@ -0,0 +1,6 @@
+
+set(QT_STEERER_VERSION 3.5.1)
+set(QT_STEERER_DEPENDS steer_lib)
+set(QT_STEERER_REPO_URL git@github.com:BlueBrain/qt_steerer.git)
+set(QT_STEERER_REPO_TAG master)
+
---
+++ config/steer_lib.cmake
@@ -0,0 +1,5 @@
+
+set(STEER_LIB_VERSION 3.5.1)
+set(STEER_LIB_REPO_URL git://github.com/RealityGrid/steer_lib.git)
+set(STEER_LIB_REPO_TAG master)
+
