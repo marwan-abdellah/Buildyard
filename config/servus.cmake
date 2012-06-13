aee0c269a45927e5a64df91e05bfaeeca84c9572
Stefan Eilemann <eilemann@gmail.com>
2012-06-13 17:33:09 +0200

    Bump versions

--- config/servus.cmake
+++ config/servus.cmake
@@ -3,7 +3,7 @@ if(NOT DNSSD_FOUND) # keep servus & gpu-sd optional
   return()
 endif()
 
-set(SERVUS_VERSION 0.9)
+set(SERVUS_VERSION 1.3.5)
 set(SERVUS_REPO_URL https://github.com/Eyescale/servus.git)
 set(SERVUS_REPO_TAG master)
 set(SERVUS_DEPENDS Lunchbox Boost)
