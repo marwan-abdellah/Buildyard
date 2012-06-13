896807e9944e47c56bed2e57e9ef3777883dd611
Stefan Eilemann <eilemann@gmail.com>
2012-06-13 17:09:12 +0200

    Temporarily remove hwloc deps to fix build

--- config/Lunchbox.cmake
+++ config/Lunchbox.cmake
@@ -2,4 +2,4 @@
 set(LUNCHBOX_VERSION 0.9.0)
 set(LUNCHBOX_REPO_URL https://github.com/Eyescale/Lunchbox.git)
 set(LUNCHBOX_REPO_TAG master)
-set(LUNCHBOX_DEPENDS Boost hwloc eyescale)
+set(LUNCHBOX_DEPENDS Boost eyescale)
