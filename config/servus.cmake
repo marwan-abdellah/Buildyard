1128f25c724f08b3118903b644bc1e46919e7f66
Stefan Eilemann <eilemann@gmail.com>
2012-06-15 11:53:08 +0200

    Update configs after servus move to Lunchbox

--- config/servus.cmake
+++
@@ -1,9 +0,0 @@
-find_package(DNSSD QUIET)
-if(NOT DNSSD_FOUND) # keep servus & gpu-sd optional
-  return()
-endif()
-
-set(SERVUS_VERSION 1.3.5)
-set(SERVUS_REPO_URL https://github.com/Eyescale/servus.git)
-set(SERVUS_REPO_TAG master)
-set(SERVUS_DEPENDS Lunchbox Boost)
