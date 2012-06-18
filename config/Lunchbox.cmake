bb2f2e97275a8d64ea7e4dd60b3f72d34dec130b
marwan-abdellah <abdellah.marwan@gmail.com>
2012-06-18 13:42:17 +0200

    Adding hwloc dependencies to Equalizer and Lunchbox

--- config/Lunchbox.cmake
+++ config/Lunchbox.cmake
@@ -2,4 +2,4 @@
 set(LUNCHBOX_VERSION 1.3.5)
 set(LUNCHBOX_REPO_URL https://github.com/Eyescale/Lunchbox.git)
 set(LUNCHBOX_REPO_TAG master)
-set(LUNCHBOX_DEPENDS Boost eyescale)
+set(LUNCHBOX_DEPENDS Boost hwloc eyescale)
