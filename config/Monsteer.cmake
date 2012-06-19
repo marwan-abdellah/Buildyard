3647e66d167059260e274375fc8420f5b3b4cc00
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2012-06-19 18:27:13 +0200

    Add Monsteer build config

---
+++ config/Monsteer.cmake
@@ -0,0 +1,5 @@
+set(MONSTEER_VERSION 0.1.0)
+set(MONSTEER_DEPENDS Boost dash Lunchbox steer_lib)
+set(MONSTEER_REPO_URL https://github.com/BlueBrain/Monsteer.git)
+set(MONSTEER_REPO_TAG master)
+set(MONSTEER_NOTEST ON)
