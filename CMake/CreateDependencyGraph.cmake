3c2747f95a7d43bcaee338320754eba26adf4f8a
Stefan Eilemann <eilemann@gmail.com>
2012-06-21 09:49:09 +0200

    Remove transitive dependencies from generated graph images

--- CMake/CreateDependencyGraph.cmake
+++ CMake/CreateDependencyGraph.cmake
@@ -1,5 +1,6 @@
 
 find_program(DOT_EXECUTABLE dot)
+find_program(TRED_EXECUTABLE tred)
 
 function(CREATE_DEPENDENCY_GRAPH_R NAME ALL FILE)
   string(TOUPPER ${NAME} UPPER_NAME)
@@ -29,12 +30,17 @@ function(CREATE_DEPENDENCY_GRAPH SRC DST NAME)
   create_dependency_graph_r(${NAME} ${dir}
     ${CMAKE_CURRENT_BINARY_DIR}/${NAME}.dot)
   file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/${NAME}.dot "}" )
-  if(NOT DOT_EXECUTABLE MATCHES "DOT_EXECUTABLE-NOTFOUND")
+  if(DOT_EXECUTABLE AND TRED_EXECUTABLE)
+    add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${NAME}_tred.dot
+      COMMAND ${TRED_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/${NAME}.dot >
+               ${CMAKE_CURRENT_BINARY_DIR}/${NAME}_tred.dot
+      DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${NAME}.dot
+      )
     add_custom_command(OUTPUT ${DST}/${NAME}.png
       COMMAND ${DOT_EXECUTABLE} -o ${DST}/${NAME}.png -Tpng
-        ${CMAKE_CURRENT_BINARY_DIR}/${NAME}.dot
-      DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${NAME}.dot eyescale
-     )
+        ${CMAKE_CURRENT_BINARY_DIR}/${NAME}_tred.dot
+      DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${NAME}_tred.dot eyescale
+      )
     add_custom_target(${NAME}-png ALL DEPENDS ${DST}/${NAME}.png)
  endif()
 endfunction()
@@ -42,11 +48,16 @@ endfunction()
 function(CREATE_DEPENDENCY_GRAPH_END SRC DST)
   get_filename_component(dir ${SRC} NAME)  
   file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/${dir}.dot "}" )
-  if(NOT DOT_EXECUTABLE MATCHES "DOT_EXECUTABLE-NOTFOUND")
+  if(DOT_EXECUTABLE AND TRED_EXECUTABLE)
+    add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${dir}_tred.dot
+      COMMAND ${TRED_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/${dir}.dot >
+              ${CMAKE_CURRENT_BINARY_DIR}/${dir}_tred.dot
+      DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${dir}.dot
+      )
     add_custom_command(OUTPUT ${DST}/all.png
       COMMAND ${DOT_EXECUTABLE} -o ${DST}/all.png -Tpng
-      ${CMAKE_CURRENT_BINARY_DIR}/${dir}.dot
-      DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${dir}.dot eyescale
+      ${CMAKE_CURRENT_BINARY_DIR}/${dir}_tred.dot
+      DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${dir}_tred.dot eyescale
       )
     add_custom_target(${dir}_png ALL DEPENDS ${DST}/all.png)
   endif()
