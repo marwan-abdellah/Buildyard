13c7524c154cad53f3ececce0bdf2a56cb16cbbe
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2012-06-25 09:50:25 +0200

    Update dependency of qt_steerer

--- config/qt_steerer.cmake
+++ config/qt_steerer.cmake
@@ -3,7 +3,7 @@ if(MSVC)
 endif()
 
 set(QT_STEERER_VERSION 3.5.1)
-set(QT_STEERER_DEPENDS steer_lib)
+set(QT_STEERER_DEPENDS Lunchbox steer_lib)
 set(QT_STEERER_REPO_URL https://github.com/BlueBrain/qt_steerer.git)
 set(QT_STEERER_REPO_TAG master)
 set(QT_STEERER_NOTEST ON)
