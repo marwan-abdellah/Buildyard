415d41882549087e941f90359da8e47d4eb55e9f
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2012-08-02 17:37:28 +0200

    Add bluebrain dep for documentation

--- config/codash.cmake
+++ config/codash.cmake
@@ -1,5 +1,5 @@
 
 set(CODASH_VERSION 0.1.0)
-set(CODASH_DEPENDS dash Collage)
+set(CODASH_DEPENDS dash Collage bluebrain)
 set(CODASH_REPO_URL https://github.com/BlueBrain/codash.git)
 set(CODASH_REPO_TAG master)
