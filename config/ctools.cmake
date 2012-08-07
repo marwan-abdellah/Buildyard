709d813800f3b23ceaee851b2a895c786262bf1f
Stefan Eilemann <eilemann@gmail.com>
2012-08-07 13:29:40 +0200

    ctools->MGL rename

--- config/ctools.cmake
+++
@@ -1,9 +0,0 @@
-if(MSVC)
-  return()
-endif()
-
-set(CTOOLS_VERSION 1.0.0)
-set(CTOOLS_REPO_URL https://github.com/ibaned/ctools.git)
-set(CTOOLS_REPO_TAG master)
-set(CTOOLS_DEPENDS bluebrain)
-set(CTOOLS_NOTEST ON)
