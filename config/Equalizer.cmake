3350e24e65ebc7f630ebf29076008e832135e96d
Stefan Eilemann <eilemann@gmail.com>
2012-08-07 11:23:00 +0200

    Tweaks to make FindPackages usable for Lunchbox

--- config/Equalizer.cmake
+++ config/Equalizer.cmake
@@ -1,7 +1,7 @@
 
 set(EQUALIZER_VERSION 1.5.0)
 set(EQUALIZER_DEPENDS REQUIRED vmmlib Lunchbox Collage OpenGL Boost
-  OPTIONAL gpu-sd hwloc GLStats)
+  OPTIONAL gpu-sd HWLOC GLStats)
 set(EQUALIZER_ROOT_VAR EQ_ROOT)
 set(EQUALIZER_REPO_URL https://github.com/Eyescale/Equalizer.git)
 set(EQUALIZER_REPO_TAG master)
--- config/Lunchbox.cmake
+++ config/Lunchbox.cmake
@@ -2,5 +2,5 @@
 set(LUNCHBOX_VERSION 1.5.0)
 set(LUNCHBOX_REPO_URL https://github.com/Eyescale/Lunchbox.git)
 set(LUNCHBOX_REPO_TAG master)
-set(LUNCHBOX_DEPENDS hwloc eyescale REQUIRED Boost)
+set(LUNCHBOX_DEPENDS HWLOC eyescale DNSSD REQUIRED Boost)
 set(LUNCHBOX_BOOST_COMPONENTS serialization)
