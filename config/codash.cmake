b10fdf1151a1bd218525404005b16e2e3bbb9bda
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2012-08-09 11:18:48 +0200

    Use FindPackages for codash

--- config/codash.cmake
+++ config/codash.cmake
@@ -1,5 +1,7 @@
 
 set(CODASH_VERSION 0.1.0)
-set(CODASH_DEPENDS bluebrain REQUIRED dash Collage)
+set(CODASH_DEPENDS bluebrain REQUIRED DASH Collage Boost)
 set(CODASH_REPO_URL https://github.com/BlueBrain/codash.git)
 set(CODASH_REPO_TAG master)
+set(CODASH_BOOST_COMPONENTS serialization)
+
