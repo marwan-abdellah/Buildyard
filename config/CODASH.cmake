fde42b8bdfd1ed2f530a0135a662d6c6a2bb7d6e
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2012-08-09 11:41:56 +0200

    Rename configs for FindPackages

--- config/CODASH.cmake
+++ config/CODASH.cmake
@@ -4,4 +4,5 @@ set(CODASH_DEPENDS bluebrain REQUIRED DASH Collage Boost)
 set(CODASH_REPO_URL https://github.com/BlueBrain/codash.git)
 set(CODASH_REPO_TAG master)
 set(CODASH_BOOST_COMPONENTS serialization)
+set(CODASH_SOURCE "${CMAKE_SOURCE_DIR}/src/codash")
 
