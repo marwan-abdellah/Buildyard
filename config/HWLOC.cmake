e46fd9cae27f7903812afedc55e5be77f5f5497d
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2012-08-10 00:14:37 +0200

    Need version for proper FindPackages creation on Windows

--- config/HWLOC.cmake
+++ config/HWLOC.cmake
@@ -1,10 +1,12 @@
+
+set(HWLOC_VERSION 1.4.0)
+
 # can't autopain on Windows without too much hassle
 # can't build universal on OS X
 if(MSVC OR APPLE)
   return()
 endif()
 
-set(HWLOC_VERSION 1.4.0)
 #set(HWLOC_REPO_URL http://svn.open-mpi.org/svn/hwloc/tags/hwloc-1.4.0/)
 set(HWLOC_REPO_URL https://github.com/marwan-abdellah/HWLOC)
 set(HWLOC_REPO_TAG display)
