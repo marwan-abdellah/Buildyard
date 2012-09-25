833451b6bf06a624b1f2f9350dfca84b62d724f0
Jafet Villafranca <jafet.villafrancadiaz@epfl.ch>
2012-09-25 11:43:15 +0200

    VRPN and Wiiuse extension

---
+++ config/VRPN.cmake
@@ -0,0 +1,8 @@
+
+set(VRPN_VERSION 07.30)
+set(VRPN_REPO_URL git://git.cs.unc.edu/vrpn.git)
+set(VRPN_REPO_TAG master)
+set(VRPN_SOURCE "${CMAKE_SOURCE_DIR}/src/vrpn")
+set(VRPN_FORCE_BUILD ON)
+set(VRPN_REPO_TYPE GIT)
+set(VRPN_OPTIONAL ON)
---
+++ config/wiiuse.cmake
@@ -0,0 +1,8 @@
+
+set(WIIUSE_VERSION 0.14)
+set(WIIUSE_REPO_URL https://github.com/jafyvilla/wiiuse.git)
+set(WIIUSE_REPO_TAG master)
+set(WIIUSE_SOURCE "${CMAKE_SOURCE_DIR}/src/wiiuse")
+set(WIIUSE_FORCE_BUILD ON)
+set(WIIUSE_REPO_TYPE GIT)
+set(WIIUSE_OPTIONAL ON)
