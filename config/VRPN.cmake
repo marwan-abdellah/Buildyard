994927778e47c7455b9a86b79d2469a959b7ee3b
Jafet Villafranca <jafet.villafrancadiaz@epfl.ch>
2012-09-27 14:03:15 +0200

    Changed the origin repository to a personalized one

--- config/VRPN.cmake
+++ config/VRPN.cmake
@@ -1,6 +1,6 @@
 
 set(VRPN_VERSION 07.30)
-set(VRPN_REPO_URL git://git.cs.unc.edu/vrpn.git)
+set(VRPN_REPO_URL https://github.com/jafyvilla/vrpn.git)
 set(VRPN_REPO_TAG master)
 set(VRPN_SOURCE "${CMAKE_SOURCE_DIR}/src/vrpn")
 set(VRPN_FORCE_BUILD ON)
