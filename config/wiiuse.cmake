a38044bdd55a9c0c661fa28532aa90c4c85a0914
Stefan Eilemann <eilemann@gmail.com>
2012-09-28 16:32:07 +0200

    Merge cleanups

--- config/wiiuse.cmake
+++ config/wiiuse.cmake
@@ -1,8 +1,5 @@
 
 set(WIIUSE_VERSION 0.14)
 set(WIIUSE_REPO_URL https://github.com/jafyvilla/wiiuse.git)
-set(WIIUSE_REPO_TAG master)
-set(WIIUSE_SOURCE "${CMAKE_SOURCE_DIR}/src/wiiuse")
-set(WIIUSE_FORCE_BUILD ON)
-set(WIIUSE_REPO_TYPE GIT)
+set(WIIUSE_FORCE_BUILD ON) # until wiiuse finder is version-aware?
 set(WIIUSE_OPTIONAL ON)
