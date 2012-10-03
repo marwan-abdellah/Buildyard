3fc7079482d68210a32cd6a74bf7cb55bcef44c3
Stefan Eilemann <eilemann@gmail.com>
2012-10-03 12:09:19 +0200

    Update wiiuse deps

--- config/wiiuse.cmake
+++ config/wiiuse.cmake
@@ -1,5 +1,6 @@
 
 set(WIIUSE_VERSION 0.14)
 set(WIIUSE_REPO_URL https://github.com/jafyvilla/wiiuse.git)
+set(WIIUSE_DEPENDS REQUIRED Bluez)
 set(WIIUSE_FORCE_BUILD ON) # until wiiuse finder is version-aware?
 set(WIIUSE_OPTIONAL ON)
