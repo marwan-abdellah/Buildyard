1b18f34f1bde9db180a262e4bce1e63311d18b48
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2012-10-16 11:23:09 +0200

    Update codash config

--- config/CODASH.cmake
+++ config/CODASH.cmake
@@ -3,6 +3,6 @@ set(CODASH_VERSION 0.1.0)
 set(CODASH_DEPENDS bluebrain REQUIRED DASH Collage Boost)
 set(CODASH_REPO_URL https://github.com/BlueBrain/codash.git)
 set(CODASH_REPO_TAG master)
-set(CODASH_BOOST_COMPONENTS serialization)
+set(CODASH_BOOST_COMPONENTS "program_options serialization")
 set(CODASH_SOURCE "${CMAKE_SOURCE_DIR}/src/codash")
 
