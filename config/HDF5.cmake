9e7e7a4194328dd6b47537e0911771ee27ea7219
Daniel Nachbaur <danielnachbaur@gmail.com>
2012-10-17 23:49:45 +0200

    Choose platform-independent download for patch file

--- config/HDF5.cmake
+++ config/HDF5.cmake
@@ -6,9 +6,13 @@ set(HDF5_SOURCE "${CMAKE_SOURCE_DIR}/src/hdf5")
 set(HDF5_OPTIONAL ON)
 
 if(CMAKE_VERSION VERSION_GREATER 2.8.5)
+  file(DOWNLOAD
+    http://www.hdfgroup.org/ftp/HDF5/current/src/cmake_patch.txt
+    ${CMAKE_BINARY_DIR}/HDF5/cmake_patch.txt
+    )
   set(HDF5_CMAKE_ARGS -DBUILD_SHARED_LIBS=ON -DHDF5_BUILD_CPP_LIB=ON -DHDF5_BUILD_HL_LIB=ON)
   set(HDF5_EXTRA
-    PATCH_COMMAND cd "${CMAKE_SOURCE_DIR}/src/hdf5" && wget http://www.hdfgroup.org/ftp/HDF5/current/src/cmake_patch.txt && patch < cmake_patch.txt
+    PATCH_COMMAND cd "${CMAKE_SOURCE_DIR}/src/hdf5" && patch < ${CMAKE_BINARY_DIR}/HDF5/cmake_patch.txt
   )
 else()
   set(HDF5_EXTRA
